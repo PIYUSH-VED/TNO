2cfe4bfad96ce7dc2273480dfa50d454e11af439
Piyush Ved <vedpiyush587@gmail.com>
2025-08-05 09:50:49 +0530

    worked on cart ui

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -24,13 +24,13 @@ const subTotal = (total - parseInt(discount) + parseInt(tax)).toFixed(2);
   return (
     <>
     <div className=" overflow-hidden h-screen flex flex-col md:flex-row  gap-2 justify-center items-center">
-        <div className='m-auto md:m-10 mt-5 pt-7 pb-6 flex flex-col overflow-y-scroll gap-4 shadow-md w-4/5 h-4/5 border-2 border-gray-200' style={{maxHeight:"100vh",scrollbarWidth:"none"}} >
+        <div className='m-auto md:m-10 mt-5 pt-7 pb-6 flex flex-col overflow-y-scroll  gap-4 shadow-md w-11/12 h-4/5 border-2 border-gray-200' style={{maxHeight:"100vh",scrollbarWidth:"none"}} >
     {cartItem.length === 0 ? ( 
     <div className='flex justify-center items-center '><h1 className='text-4xl '>Your Cart is empty</h1></div>
  ) : (
      cartItem.map((item) => (
           <div className="menu-item flex flex-row bg-white border-amber-50  md:w-11/12 h-[125px] shadow-2xl mr-2.5 ml-1 border-b-2 items-center" key={item.id}>
-            <div className=" h-30 w-50 flex p-2">
+            <div className=" h-25 w-40 md:h-30 md:w-50 flex p-2">
               <img
                 src={item.icon}
                 alt={item.name}
@@ -41,10 +41,11 @@ const subTotal = (total - parseInt(discount) + parseInt(tax)).toFixed(2);
             <div className='flex flex-col justify-between w-full h-full p-2'>
               <h5 className=''>{item.name}</h5>
               <h6>₹{item.price * item.quantity }</h6>
-              <h5>{item.quantity}</h5>
+  
               <div className='flex items-center gap-2 flex-row'>
-              <button className='text-[30px]' onClick={()=> incquantity(item)}> +</button>
-              <button className='text-[30px]' onClick={()=> decquantity(item)}> -</button>
+              <button className='text-[30px] bg-gray-100 w-[40px] h-[30px]  items-center flex justify-center  shadow-sm' onClick={()=> decquantity(item)}> -</button>
+              <h5>{item.quantity}</h5>
+              <button className='text-[30px] bg-gray-100 w-[40px] h-[30px] items-center flex justify-center shadow-sm' onClick={()=> incquantity(item)}> +</button>
               <button className='' onClick={() => removeitem(item)}> <Trash2 /> </button>
               </div>
             </div>
@@ -52,7 +53,7 @@ const subTotal = (total - parseInt(discount) + parseInt(tax)).toFixed(2);
         ))
     )}
       </div>
-      <div className='m-auto md:m-10 mt-5 pt-7 pb-6 flex flex-col shadow-2xl overflow-y-scroll gap-4 w-4/5 md:w-2/5 lg:w-2/5 xl:w-2/5 p-2 h-4/5 border-2 border-gray-200' style={{maxHeight:"100vh",scrollbarWidth:"none"}}>
+      <div className='m-auto md:m-10 mt-5 pt-7 pb-6 flex flex-col shadow-2xl overflow-y-scroll gap-4 w-11/12 md:w-2/5 lg:w-2/5 xl:w-2/5 p-2 h-4/5 border-2 border-gray-200' style={{maxHeight:"100vh",scrollbarWidth:"none"}}>
         <h1 className='text-2xl font-bold text-center mb-6 text-gray-800'>Bill</h1>
         <div className='flex flex-row justify-between items-center gap-10 w-full left-0'>
             <span>Total:</span>
